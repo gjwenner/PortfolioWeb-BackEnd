67fd42a17c13a89e6c4d508c91225f9d8afb0b51
gjwenner <gjwenner@gmail.com>
2023-05-18 19:56:19 -0300

    master

--- src/main/java/com/portfolio/gjw/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/gjw/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://backendgjw-gvbg.onrender.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
